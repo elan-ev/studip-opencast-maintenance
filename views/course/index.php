b19e2c8b319d1f0a59deffb9eab8dca693f120cb
Till Glöggler <till@gundk.it>
2020-05-07 12:54:40 +0200

    fix wording

--- views/course/index.php
+++ views/course/index.php
@@ -1,2 +1,2 @@
 <?= MessageBox::warning('Auf Grund von dringenen Wartungsarbeiten im Rechenzentrum steht am
-Freitag den 08.05.2020 das Opencast-Video-System in der Zeit von 7:00 Uhr und 8:00 Uhr nicht zur Verfügung.') ?>
+Freitag den 08.05.2020 das Opencast-Video-System in der Zeit von 7:00 Uhr bis 8:00 Uhr nicht zur Verfügung.') ?>
